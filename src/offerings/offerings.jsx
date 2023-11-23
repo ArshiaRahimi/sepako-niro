21f904b574580543a73715a31912e10e112bc94e
Arshia <arshiarahimi78@gmail.com>
2023-11-23 17:46:37 +0330

    fixed offerings

--- src/offerings/offerings.jsx
+++ src/offerings/offerings.jsx
@@ -3,14 +3,22 @@ import './offerings.css'
 
 const offerings = () => {
   return (
-    <div className='offerings'>
-      <div class="square">طراحی و اجراي سیستم هاي اتوماسیون مربوط بویلرهاي صنعتی</div>
-      <div class="square">طراحی و اجراي سیستم هاي اتوماسیون مربوط به توربین هاي بخار و گاز</div>
-      <div class="square">طراحی و اجراي سیستم هاي اتوماسیون مربوط به کنترل شبکه هاي برق PDCS</div>
-      <div class="square">تامین تجهیزات ابزار دقیق در صنایع نفت ، گاز ، پتروشیمی و صنایع سنگین .</div>
-      <div class="square">تامین تجهیزات ضد انفجار</div>
-      <div class="square">مشاور و مجري پروژه هاي برق</div>
+    <div>
+      <div class="about">خدمات</div>
+      <div className='offerings'>
+        <div class="firstRow">
+          <div class="square">طراحی و اجراي سیستم هاي اتوماسیون مربوط بویلرهاي صنعتی</div>
+          <div class="square">طراحی و اجراي سیستم هاي اتوماسیون مربوط به توربین هاي بخار و گاز</div>
+          <div class="square">طراحی و اجراي سیستم هاي اتوماسیون مربوط به کنترل شبکه هاي برق PDCS</div>
+        </div>
+        <div class="secondRow">
+          <div class="square">تامین تجهیزات ابزار دقیق در صنایع نفت ، گاز ، پتروشیمی و صنایع سنگین .</div>
+          <div class="square">تامین تجهیزات ضد انفجار</div>
+          <div class="square">مشاور و مجري پروژه هاي برق</div>
+        </div>
     </div>
+    </div>
+    
   )
 }
 
