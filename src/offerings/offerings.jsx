02721e9dddf5cb62082b5c509c975db08a615cee
Arshia <arshiarahimi78@gmail.com>
2023-11-23 22:48:58 +0330

    add icons to offerings section

--- src/offerings/offerings.jsx
+++ src/offerings/offerings.jsx
@@ -7,13 +7,13 @@ const offerings = () => {
       <div class="about">خدمات</div>
       <div className='offerings'>
         <div class="firstRow">
-          <div class="square">طراحی و اجراي سیستم هاي اتوماسیون مربوط بویلرهاي صنعتی</div>
-          <div class="square">طراحی و اجراي سیستم هاي اتوماسیون مربوط به توربین هاي بخار و گاز</div>
+          <div class="square" id="s1">طراحی و اجراي سیستم هاي اتوماسیون مربوط بویلرهاي صنعتی</div>
+          <div class="square" id="s2">طراحی و اجراي سیستم هاي اتوماسیون مربوط به توربین هاي بخار و گاز</div>
           <div class="square">طراحی و اجراي سیستم هاي اتوماسیون مربوط به کنترل شبکه هاي برق PDCS</div>
         </div>
         <div class="secondRow">
           <div class="square">تامین تجهیزات ابزار دقیق در صنایع نفت ، گاز ، پتروشیمی و صنایع سنگین .</div>
-          <div class="square">تامین تجهیزات ضد انفجار</div>
+          <div class="square" id="s5">تامین تجهیزات ضد انفجار</div>
           <div class="square">مشاور و مجري پروژه هاي برق</div>
         </div>
     </div>
